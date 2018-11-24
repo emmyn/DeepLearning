a06a203550316dd9c7d7afbe2f55fc8405472c57
Emmy Nadeau <emmy.nadeau@polymtl.ca>
2018-11-23 20:38:43 -0500

    Ajout des templates de preprocessing et de classification

---
+++ Templates/data_preprocessing.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv("file.csv")
+
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
